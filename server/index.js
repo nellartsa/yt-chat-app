3c351153c32f954f9165e10fe1a93f4cb5538354
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-03-13 16:35:42 +0800

    Project Started

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express()
+require("dotenv").config()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("DB connection successful")
+}).catch((e) => {
+    console.log(e.message)
+})
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on Port ${process.env.PORT}`)
+})
+
